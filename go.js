4169042e82a95cb16f2fab8b8c37d9120153df61
Robert Tompkins <rtompkins@glgroup.com>
2014-05-01 14:43:57 -0400

    added querying and test queries to node code

--- go.js
+++ go.js
@@ -2,20 +2,32 @@ var elasticsearch = require('elasticsearch');
 
 // Connect to localhost:9200 and use the default settings
 var client = new elasticsearch.Client();
+function search(fieldName,fieldValue)
+{
+	// search for documents (and also promises!!)
+	var searchObject = {index: 'ci', size: 50, body: {query: {match: {}}}};
+	searchObject.body.query.match[fieldName] = fieldValue;
+	client.search(searchObject).then(function (resp) {
+	for (var i=0;i<resp.hits.hits.length;i++)
+	{ 
+	  console.log(resp.hits.hits[i]._source);
+	}
 
-// search for documents (and also promises!!)
-client.search({
-  index: 'ci',
-  size: 50,
-  body: {
-    query: {
-      match: {
-       cname: '李'
-      }
-    }
-  }
-}).then(function (resp) {
-  console.log(resp.hits.hits.length);
-  console.log(resp.hits.hits);
 
-});
+	});
+}
+
+
+
+//search('common','你');
+console.log('Searching on Last Name:' + '朱')
+search('cname','朱');
+
+console.log('Searching on First Name:' + '庆')
+search('cname','庆');
+
+console.log('Searching on Full Name:' + '朱庆')
+search('cname','庆');
+
+console.log('Searching on Complex Name:' + '生')
+search('cname','生');
